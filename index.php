c2d83bb80157e4fce841b6e93f0e1810debca7a5
masoud harooni <masoudharooni50@gmail.com>
2021-08-18 00:58:25 +0430

    bulid-structure-project

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include "bootstrap/init.php";
+
+// use Hekmatinasser\Verta\Verta;
+
+
+// $v = Verta();
+// $v->timezone = 'Asia/Baku';
+// echo ($v);
+
+include "views/tpl-index.php";
--- views/tpl-index.php
+++ views/tpl-index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>CodePen - Task manager UI</title>
-  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 
 </head>
 <body>
@@ -71,7 +71,7 @@
   </div>
 </div>
 <!-- partial -->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="assets/js/script.js"></script>
 
 </body>
 </html>
