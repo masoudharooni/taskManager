4dfe3e95bee23ce708934a639beb96d4f590c36a
masoud harooni <masoudharooni50@gmail.com>
2021-08-17 20:03:00 +0430

    add-static-ui

---
+++ script.js
@@ -0,0 +1,10 @@
+(function() {
+  /*
+Inspired by dribble.com/shots/1507858-Dashboard
+*/
+
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7RUFBQTs7OztBQUFBIiwic291cmNlc0NvbnRlbnQiOlsiIyMjXG5JbnNwaXJlZCBieSBkcmliYmxlLmNvbS9zaG90cy8xNTA3ODU4LURhc2hib2FyZFxuIyMjIl19
+//# sourceURL=coffeescript
